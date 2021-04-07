1ae571ec9818a1edaf575c3c33fd94345c74b27e
wackoz <francescovacc4@gmail.com>
2021-04-07 11:55:40 +0200

    "Lab04 examples updated"

---
+++ class_examples/lab04/ex01-shmP-receiver.c
@@ -0,0 +1,30 @@
+#include <errno.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+#define SIZE (2 * sizeof(int))
+#define NAME "/shmem"
+
+int main() {
+  int fd, i;
+  int *data;
+
+  fd = shm_open(NAME, O_CREAT | O_RDONLY, 0666);
+  if (fd < 0) {
+    perror("shm_open:");
+    exit(-1);
+  }
+  data = (int *)mmap(0, SIZE, PROT_READ, MAP_SHARED, fd, 0);
+
+  for (i = 0 ; i < 2; i++) {
+    printf("Reading from the shared memory: The data[%d]: %d\n", i, data[i]);
+  }
+  munmap(data, SIZE);
+  shm_unlink(NAME);
+  return 0;
+}
--- class_examples/lab04/ex01-shmP-sender.c
+++ class_examples/lab04/ex01-shmP-sender.c
@@ -8,33 +8,33 @@
 #include <unistd.h>
 
 #define SIZE (2 * sizeof(int))
-#define NAME "/shmem"  
-// Program to share ana array of integer with the sisze os 2 >> int data[2] between two processes
+#define NAME "/shmem"
+// Program to share ana array of integer with the sisze os 2 >> int data[2]
+// between two processes
 
 int main() {
-    int fd, i;
-    int *data;
+  int fd, i;
+  int *data;
 
-    fd = shm_open(NAME, O_CREAT | O_RDWR | O_EXCL, 0666);
-        
-    if (fd<0){
-        perror("shm_open: ");
-        exit(-1);
-    }
+  if ((fd = shm_open(NAME, O_CREAT | O_RDWR | O_EXCL, 0666) < 0)) {
+    perror("shm_open: ");
+    exit(-1);
+  }
+  
+  ftruncate(fd, SIZE); //length of the virtual file
+  
+  data = (int *)mmap(0, SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
 
-    ftruncate(fd,SIZE);
-    data = (int *) mmap(0, SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0 );
-    if (data < 0){
-        perror("mmap: ");
-    }
+  if (data < 0) {
+    perror("mmap: ");
+  }
 
-    for(i = 0; i<2; i++){
-        data[i] = i + 10;
-        printf("writing in the sahred memory: data[%d]: %d\n", i, data[i]);
-    }
+  for (i = 0; i < 2; i++) {
+    data[i] = i + 10;
+    printf("Writing in the shared memory: data[%d]: %d\n", i, data[i]);
+  }
 
-
-    munmap(data, SIZE);
-    close(fd);
-    return 0;
+  munmap(data, SIZE); //unmap
+  close(fd); //we don't unlink, a receiver have to read the shared mem
+  return 0;
 }
